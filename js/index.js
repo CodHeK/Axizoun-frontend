ac3d5ada371c29b227d0ca8bee3cf357c5cc605a
CodHeK <gaganganapathyas@yahoo.in>
2018-07-22 18:00:58 +0530

    login modal added

--- js/index.js
+++ js/index.js
@@ -14,4 +14,38 @@ $(document).ready(function() {
     $("#employee-form").show(500);
     $("#employee").addClass('life');
   });
+
+  $("#m-employer-form").hide();
+  $("#m-employee").addClass('life');
+
+  $("#m-employer").click(function() {
+    $("#m-employee-form").hide();
+    $("#m-employee").removeClass('life');
+    $("#m-employer-form").show(500);
+    $("#m-employer").addClass('life');
+  });
+  $("#m-employee").click(function() {
+    $("#m-employer-form").hide();
+    $("#m-employer").removeClass('life');
+    $("#m-employee-form").show(500);
+    $("#m-employee").addClass('life');
+  });
+
+  $("#m1-employer-form").hide();
+  $("#m1-employee").addClass('life');
+
+  $("#m1-employer").click(function() {
+    $("#m1-employee-form").hide();
+    $("#m1-employee").removeClass('life');
+    $("#m1-employer-form").show(500);
+    $("#m1-employer").addClass('life');
+  });
+  $("#m1-employee").click(function() {
+    $("#m1-employer-form").hide();
+    $("#m1-employer").removeClass('life');
+    $("#m1-employee-form").show(500);
+    $("#m1-employee").addClass('life');
+  });
+
+
 });
